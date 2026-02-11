50717eabff3a760da4e3807be72a155c3c2ae176
Nutan <sharmanutan358@gmail.com>
2026-02-11 16:37:02 +0530

    Initial commit - EmployeeAdminPortal API

---
+++ EmployeeAdminPortal/Program.cs
@@ -0,0 +1,28 @@
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
